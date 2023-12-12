80ef1405cfb1a8b47312da375a8a7eb668968b76
Dan <drnw20@bath.ac.uk>
2023-12-12 18:42:22 +0000

    deadend, changing tack

--- backend.py
+++ backend.py
@@ -1,12 +1,84 @@
+import sqlite3
+from sqlite3 import Error
+from datetime import date
+import database_setup
 BORROW_LENGTH=21
+PATH="C:\\Users\\danwh\\OneDrive\\Documents\\python\\Viable-Library\\library_database.db"
+user_id=0
+#taken from tutorial
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
 
-
-def borrow_book(user, copy):
-
-def return_book(user, copy):
+def borrow_book(copy):
+    #validate user is logged in
+    #validate copy is available
+    borrow_update="""
+    UPDATE copy
+    SET borrower_id= %d
+    due_date = %s 
+    WHERE id = %d"""%(user_id,date.today()+BORROW_LENGTH,copy)
+    
+def return_book(copy):
+    pass
 
 def get_book_list(filter=None):
+    
+    query_book_list="""
+    SELECT 
+        books.title,
+        books.description,
+        copy.available,
+        copy.id
+    FROM 
+        copy
+        INNER JOIN books on books.id = copy.book_id
+    """
+    if filter=="available":
+        query_book_list+=" WHERE copy.available = TRUE"
+    elif filter=="today":
+        query_book_list+= " WHERE copy.due_date = %s"%date.today()
+    elif filter=="loan":
+        query_book_list="""
+        SELECT 
+            books.title,
+            books.description,
+            copy.id,
+            copy.due_date,
+            user.first_name,
+            user.last_name,
+            user.email,
+        FROM 
+            copy
+            INNER JOIN books on books.id = copy.book_id
+            INNER JOIN users on user.id = copy.borrower_id
+        """
+    print(execute_read_query(con, query_book_list))
+
+def register_user(first,last,email):
+    #email_exists_query="""SELECT email FROM users WHERE email = '%s'"""% email
+    #add validation of email format and if there already is an account.
+    insert_user="""INSERT INTO users
+    VALUES
+    ('%s','%s','%s')"""%(first,last,email)
+    database_setup.execute_query(con,insert_user)
+
+
+def login(email):
+    user_id=database_setup.execute_query(con,"SELECT id FROM users WHERE email = '%s"%email)
+    #add error if email not in database
+
+
+
+con=database_setup.setup(PATH)
+get_book_list()
 
 
 
--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,55 @@
 import sqlite3
 from sqlite3 import Error
 
+
+
+create_books_table = """
+CREATE TABLE IF NOT EXISTS books (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT NOT NULL,
+description TEXT NOT NULL
+);"""
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT NOT NULL, 
+last_name TEXT NOT NULL, 
+email TEXT NOT NULL
+);"""
+
+create_copy_table = """
+CREATE TABLE IF NOT EXISTS copy (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+available BOOLEAN,
+borrower_id INTEGER,
+due_date TEXT,
+book_id INTEGER NOT NULL,
+FOREIGN KEY (borrower_id) REFERENCES users(id),
+FOREIGN KEY (book_id) REFERENCES books(id)
+);"""
+
+
+create_users = """
+INSERT INTO users (first_name, last_name, email)
+VALUES
+('John', 'Smith', 'John@Smith.com'),
+('Joe', 'Bloggs', 'Josephblogger@gmail.com');
+"""
+create_books = """
+INSERT INTO books (title, description)
+VALUES
+('Return of the King', 'The epic finale in the genre defining work by J.R.R Tolkien'),
+('Foundation', 'The Galactic Empire is doomed but the Encyclopedists of Terminus may shorten the turmoil');
+"""
+create_copy = """
+INSERT INTO copy (available, borrower_id, due_date, book_id)
+VALUES
+(1, NULL, NULL, 1),
+(1, NULL, NULL, 1),
+(1, NULL, NULL, 2);
+"""
+
 def create_connection(path):
     connection = None
     try:
@@ -20,37 +69,16 @@ def execute_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-connection = create_connection("C:\\Users\\danwh\\OneDrive\\Documents\\python\\Viable-Library\\library_database.db")
-print("got here")
-create_books_table = """
-CREATE TABLE IF NOT EXISTS books (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT NOT NULL,
-description TEXT NOT NULL
-);"""
 
-create_users_table = """
-CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-first_name TEXT NOT NULL, 
-last_name TEXT NOT NULL, 
-email TEXT NOT NULL, 
-password TEXT NOT NULL
-);"""
 
-create_copy_table = """
-CREATE TABLE IF NOT EXISTS copy (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-available BOOLEAN,
-borrower_id INTEGER NOT NULL,
-book_id INTEGER NOT NULL,
-FOREIGN KEY (borrower_id) REFERENCES users(id),
-FOREIGN KEY (book_id) REFERENCES books(id)
-);"""
+def setup(path):
+    connection = create_connection(path)
+    execute_query(connection, create_users_table)
+    execute_query(connection, create_books_table)
+    execute_query(connection, create_copy_table)
 
-execute_query(connection, create_users_table)
-execute_query(connection, create_books_table)
-execute_query(connection, create_copy_table)
-
-#put data in the tables here
-
+    #put data in the tables here
+    execute_query(connection, create_users)
+    execute_query(connection, create_books)
+    execute_query(connection, create_copy)
+    return connection
