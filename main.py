48f777ffeb9ede6056f4eabf33ddfd3eb5965faa
Dan <drnw20@bath.ac.uk>
2023-12-13 10:46:03 +0000

    added review of work

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from viablelibrary import ViableLibrary
+
+
 def create_values(lib):
-    books=[["Return of the King","The finale to the epic, genre defining work by J.R.R. Tolkien."],
-           ["Foundation", "The Galactic Empire is dying but Hari Seldon's Psychohistory predicts a better future."],
-           ["Artemis Fowl", "Teenage criminal mastermind begins to pray on the fay, but does he bite off more than he chew?"]]
+    books = [
+        [
+            "Return of the King",
+            "The finale to the epic, genre defining work by J.R.R. Tolkien.",
+        ],
+        [
+            "Foundation",
+            "The Galactic Empire is dying but Hari Seldon's Psychohistory predicts a better future.",
+        ],
+        [
+            "Artemis Fowl",
+            "Teenage criminal mastermind begins to pray on the fay, but does he bite off more than he chew?",
+        ],
+    ]
     for b in books:
-        lib.add_book(b[0],b[1])
+        lib.add_book(b[0], b[1])
 
 
-lib=ViableLibrary()
+lib = ViableLibrary()
 create_values(lib)
+
+# very crude demonstration. Unfortunately ran out of time building a web-based UI.
 print(lib.get_available_books())
-print(lib.register("John", "Smith", "johnsmith@mail.com","pass"))
+print(lib.register("John", "Smith", "johnsmith@mail.com", "pass"))
 print(lib.login("johnsmith@mail.com", "pass"))
 print(lib.borrow("Foundation"))
 print(lib.get_book_list())
 print(lib.get_available_books())
-print(lib.get_loaned_books())            
-
+print(lib.get_loaned_books())
--- viablelibrary.py
+++ viablelibrary.py
@@ -4,13 +4,16 @@ import datetime
 
 
 class ViableLibrary:
+    """Create a blank instance of ViableLibrary."""
+
     def __init__(self) -> None:
         self.books = {}  # title as key
         self.users = {}  # email as key
         self.current_user = None
         self.borrow_time = 21
 
-    def register(self, first_name, last_name, email, password):
+    def register(self, first_name, last_name, email, password) -> bool:
+        """Register a new user and return a bool."""
         if email not in self.users:
             self.users[email] = User(first_name, last_name, email, password)
             return True
@@ -18,6 +21,7 @@ class ViableLibrary:
             return False
 
     def login(self, email, password):
+        """Check if email and password is a valid combination and return bool."""
         self.logout()
         if email in self.users and self.users[email].login(password):
             self.current_user = self.users[email]
@@ -25,29 +29,35 @@ class ViableLibrary:
         return False
 
     def logout(self):
+        """Clear current user and return bool."""
         if self.current_user:
             self.current_user.logout()
         self.current_user = None
         return True
 
     def borrow(self, title):
+        """Check out book and return due date if successful, False otherwise."""
         if self.current_user and self.books[title].available:
             self.books[title].borrow(self.current_user.email, self.borrow_time)
-            # update something in current user?
-            return self.books[title].due_date
-        return False
+            return self.books[
+                title
+            ].due_date  # currently this returns either a date or a bool
+        return False  # while a date will evaluate to True, this is not very clean.
+        # Potentially refactor all False returns to exceptions
 
     def return_book(self, title):
+        """Mark book as not on loan and return bool."""
         if self.current_user and not self.books[title].available:
             self.books[title].return_book()
-            # update current user?
             return True
         return False
 
     def get_book_list(self):
+        """Return the list of books."""
         return [self.books[title].get_basic_details() for title in self.books]
 
     def get_available_books(self):
+        """Return the list of books avaiable for borrowing."""
         return [
             self.books[title].get_basic_details()
             for title in self.books
@@ -55,6 +65,7 @@ class ViableLibrary:
         ]
 
     def get_due_books(self):
+        """Return the list of books due back today."""
         return [
             self.books[title].get_basic_details()
             for title in self.books
@@ -62,17 +73,19 @@ class ViableLibrary:
         ]
 
     def get_loaned_books(self):
-        temp = [
+        """Return the list of books currently on loan with borrower details."""
+        withoutNames = [
             self.books[title].get_full_details()
             for title in self.books
             if not self.books[title].available
         ]
         return [
-            book + self.users[book[-1]].get_names() for book in temp
-        ]  # refactor for clarity. This adds borrower's names
+            book + self.users[book[-1]].get_names() for book in withoutNames
+        ]  # Here we need to add additional information about the borrower
 
     def add_book(self, title, description, image=None):
+        """Add a book to the library dataset and return bool"""
         if title not in self.books:
-            self.books[title]=Book(title,description,image)
+            self.books[title] = Book(title, description, image)
             return True
         return False
